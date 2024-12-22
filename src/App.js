f06a0ab6133b2b9bcbbad2d10694b37cc939c59b
MisalSilva <misal.20230183@iit.ac.lk>
2024-12-22 20:49:51 +0530

    Remove commented-out code from App component for cleaner structure

--- src/App.js
+++ src/App.js
@@ -15,6 +15,21 @@ function App() {
   //     <h2>Submit Responses</h2>
   //   </div>
   // );
+
+// function App() 
+//   return (
+//     <div className="App">
+//       <h1>Welcome to the Estate Agent Site</h1>
+//       <h2>Client Side Web Application</h2>
+//       <h2>Search your property</h2>
+//       <h2>Submit Responses</h2>
+//     </div>
+//   );
+
+
+
+
+
 }
 
 export default App;
