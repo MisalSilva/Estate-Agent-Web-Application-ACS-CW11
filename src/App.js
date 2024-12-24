2099aa55897ab3a6f60c8aa2fe7264dc32e3c5a8
MisalSilva <misal.20230183@iit.ac.lk>
2024-12-24 23:47:54 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -8,13 +8,6 @@ function App() {
       <Navbar/>
     </div>
   );
-  //   <div className="App">
-  //     <h1>Welcome to the Estate Agent Site</h1>
-  //     <h2>Client Side Web Application</h2>
-  //     <h2>Search your property</h2>
-  //     <h2>Submit Responses</h2>
-  //   </div>
-  // );
 
 // function App() 
 //   return (
