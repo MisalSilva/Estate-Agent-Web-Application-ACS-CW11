602f2c9dd493739cc382b0a20c9b32471e983c5d
MisalSilva <misal.20230183@iit.ac.lk>
2024-12-25 22:22:36 +0530

    Update Navbar links for improved navigation and clarity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,14 @@ function Navbar() {
     return (
         <nav className="navbar">
             <div className="navbar-logo">
-            <a href="/">
+            <a href="Home.js">
                     <img src={NestHubLogo1} alt="NestHub Logo" />
                 </a>
             </div>
             <ul className="navbar-links">
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/services">Services</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="/about">Search Properties</a></li>
+                <li><a href="/">Favourites</a></li>
             </ul>
             <div className="navbar-login">
                 <a href="/login">Login</a>
