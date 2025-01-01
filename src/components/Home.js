96c0d111e68297d81c086c79b1381badcc6cc715
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-01 14:59:15 +0530

    Update button elements in Home.js for improved navigation and functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,13 +10,13 @@ function Home() {
                     Manage all your estate-related tasks efficiently and with ease. 
                     Our system ensures transparency, speed, and accuracy in every step.
                 </p>
-                <button className="explore-button">Explore Now</button>
+                <button className="explore-button"><a href='search-content'>Explore Now</a></button>
             </div>
             <div className="search-content">
                 <p>Search properties, ensure it's upto your requirements and contact the agents</p>
                 {/* <input type="text" placeholder="Search for properties" />
                 <button className="search-button">Search</button> */}
-                <button className="explore-button">Search Now</button>                
+                <button className="search-button">Search Now</button>                
             </div>
         </div>
     );
