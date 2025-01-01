04a373d4681b22572809f8ab1664b2b52d169d0a
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-01 23:48:25 +0530

    Implement routing in App component for Home, Search, and Result pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Search from './components/Search';
+import Result from './components/Result';
 
 function App() {
-  return (
-    <div>
-      <Navbar/>
-      <Home/>
-    </div>
-  );
-
-
-
-
-
-
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/results" element={<Result />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
