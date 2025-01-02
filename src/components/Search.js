7671d9dc70220f86ef11efbfcd4074b3e704b8b5
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-02 14:14:39 +0530

    Add Search component styles import

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Search.css';
 import propertiesData from '../data/properties.json'; // Import the JSON file
 
 const Search = () => {
