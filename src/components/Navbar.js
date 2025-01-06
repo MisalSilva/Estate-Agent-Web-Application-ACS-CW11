dec19d1aafc9544e57199313fd8a379a5c7a6c37
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-06 10:56:15 +0530

    Refactor Navbar component: add mobile menu toggle functionality and update structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NestHubLogo1 from './NestHubLogo1.png'; // Make sure to import the logo
 import '../styles/Navbar.css';
-import { useNavigate } from 'react-router-dom';    
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
     const navigate = useNavigate();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <nav className="navbar">
-            <div className="navbar-logo">
-            <a href="Home.js">
-                    <img src={NestHubLogo1} alt="NestHub Logo" />
-                </a>
+            <div className="navbar-container">
+                <div className="navbar-logo">
+                    <a href="Home.js">
+                        <img src={NestHubLogo1} alt="NestHub Logo" />
+                    </a>
+                </div>
+                <button className="navbar-toggle" onClick={toggleMenu}>
+                    <span className="navbar-toggle-icon">&#9776;</span>
+                </button>
+                <ul className={`navbar-links ${isMenuOpen ? 'navbar-links-active' : ''}`}>
+                    <li><a href="/" onClick={() => navigate('/')}>Home</a></li>
+                    <li><a href="/search" onClick={() => navigate('/search')}>Search Properties</a></li>
+                </ul>
             </div>
-            <ul className="navbar-links">
-                <li><a href="/" onClick={() => navigate('/')}>Home</a></li>
-                <li><a href="/search" onClick={() => navigate('/search')}>Search Properties</a></li>
-            </ul>
         </nav>
     );
 };
